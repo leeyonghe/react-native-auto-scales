5074665042854b9db8d54f05e205b70802cd4ae5
lee <lyhdra99@svcorps.dev>
2020-05-23 10:20:12 +0900

    init

---
+++ index.js
@@ -0,0 +1,40 @@
+import { Dimensions, ImageStyle, StyleSheet, PixelRatio, TextStyle, ViewStyle } from "react-native";
+const { width, height } = Dimensions.get("window");
+const dimensions = width < height ? width : height;
+
+const guidelineBaseWidth = 350;
+let shouldScale = true;
+
+export function scale(size: number) {
+  return PixelRatio.roundToNearestPixel((dimensions / guidelineBaseWidth * size));
+}
+
+// export function shouldScale(shouldScaleSize: boolean) {
+//   shouldScale = shouldScaleSize;
+// }
+
+export default {
+  ...StyleSheet,
+
+  create(styles, disableScale = []) {
+    const newStyles = {};
+
+    for (const key in styles) {
+      let style: ViewStyle | TextStyle | ImageStyle = styles[key];
+      newStyles[key] = { ...style };
+
+      for (const property in style) {
+        const value = style[property];
+        const name = String(newStyles[key][property]);
+
+        if (typeof value === 'number' && !name.includes("flex") && !disableScale.includes(name) && shouldScale) {
+          newStyles[key][property] = scale(value);
+        }
+      }
+    }
+
+    return StyleSheet.create(newStyles);
+  },
+
+  createNoscaled: StyleSheet.create
+};
